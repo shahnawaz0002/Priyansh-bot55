e9cb74fe581ebe5f123976c7629afb08ec28e768
shahnawaz0002 <shahnawazsidd9696@gmail.com>
2025-06-15 22:50:45 +0530

    Update 0tea.js

--- Priyansh/commands/0tea.js
+++ Priyansh/commands/0tea.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
     var msg = {
-        body: "Ye Lo Bby ☕",
-        attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
+        body: "Ye Lo Bby garma garam or shanu ko janu bolo ☕",
+        attachment: fs.createReadStream(__dirname + `/noprefix/chai1.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫖", event.messageID, (err) => {}, true)
